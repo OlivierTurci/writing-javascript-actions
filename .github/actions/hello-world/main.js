9f634a5750a0c638010146f2d7f9d4cfae97a43c
Olivier Turci <olivier.turci@weinvest.be>
2021-03-29 11:37:24 +0200

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const core = require("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
+
+firstGreeting
+  ? console.log(`Hello ${firstGreeting}`)
+  : console.log(`first-greeting Missing`);
+secondGreeting
+  ? console.log(`Hello ${secondGreeting}`)
+  : console.log(`second-greeting Missing`);
+thirdGreeting
+  ? console.log(`Hello ${secondGreeting}`)
+  : console.log(`third-greeting Missing`);
